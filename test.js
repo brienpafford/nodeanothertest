49750e3c709a6a659cc99bbd06115d94142e669e
Brien Pafford <bcpaff@gmail.com>
2015-07-22 13:32:08 -0500

    Travis cli works, for in class code

--- test.js
+++ test.js
@@ -1,81 +1,9 @@
 var path = require('path');
 var should = require("chai").should();
 var Animal = require(path.join(process.cwd(), '/lib/Animal'));
-// var argv = require(path.join(process.cwd(), 'app'));
 var cp = require('child_process');
 
-// var expect = require('chai').expect;
-
-
-// describe('Mocha + Chai', function() {
-//   it('truthiness', function () {
-//     true.should.equal(true);
-//   });
-// });
-
-// describe('CLI', function() {
-//   it('should work for a one-word name and capitalize all letters despite being put in in lowercase', function (done) {
-//     cp.execFile('/lib/cheers.js', ["mari"], function (err, stdout) {
-//       stdout.should.equal('Give me an M!\nGive me an A!\nGive me an R!\nGive me an I!\n')
-//       done();
-//     })
-//   });
-
-//   it('should insert an instead of a for any letter in halfnorsemix', function (done) {
-//     cp.execFile('/lib/cheers.js', ["halfnorsemix"], function (err, stdout) {
-//       stdout.should.equal('Give me an H!\nGive me an A!\nGive me an L!\nGive me an F!\nGive me an N!\nGive me an O!\nGive me an R!\nGive me an S!\nGive me an E!\nGive me an M!\nGive me an I!\nGive me an X!\n')
-//       done();
-//     })
-//   });
-
-//   it('should work for a multi-word name and not insert spaces into the cheers', function (done) {
-//     cp.execFile('/lib/cheers.js', ["bob", "marley"], function (err, stdout) {
-//       //console.log(err);
-//       stdout.should.equal('Give me a B!\nGive me an O!\nGive me a B!\nGive me an M!\nGive me an A!\nGive me an R!\nGive me an L!\nGive me an E!\nGive me a Y!\n')
-//       done();
-//     })
-//   });
-
-//   it('should work even if the name is put in in all caps', function (done) {
-//     cp.execFile('/lib/cheers.js', ["MARI"], function (err, stdout) {
-//       stdout.should.equal('Give me an M!\nGive me an A!\nGive me an R!\nGive me an I!\n')
-//       done();
-//     })
-//   });
-
-//   it('should work with nonenglish/extended characters', function (done) {
-//     cp.execFile('/lib/cheers.js', ["中高田馬場"], function (err, stdout) {
-//       stdout.should.equal('Give me a 中!\nGive me a 高!\nGive me a 田!\nGive me a 馬!\nGive me a 場!\n')
-//       done();
-//     })
-//   });
-
-//   it('should work with nonalphabet characters', function (done) {
-//     cp.execFile('/lib/cheers.js', ["$#&@"], function (err, stdout) {
-//       stdout.should.equal('Give me a $!\nGive me a #!\nGive me a &!\nGive me a @!\n')
-//       done();
-//     })
-//   });
-
-// });
-
-// describe('CLI', function () {
-//   it('should thank me for downloading', function (done) {
-//     console.log('childprocess');
-//     cp.execFile('./app.js', function (err, stdout) {
-//       console.log('err', err);
-//       console.log('stdout', stdout);
-//       stdout.should.equal('Thank for Downloading me\n')
-//       done();
-//     });
-//   });
-// });
-
-
-
-// TESTING AN 'ANIMAL' OBJECT
-
-
+//TESTING AN 'ANIMAL' OBJECT
 
 
 describe('animal', function () {
